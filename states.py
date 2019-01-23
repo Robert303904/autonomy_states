7f39b64f3c61f4f0a360abcf7953277d8bb77c79
Robert303904 <32463227+Robert303904@users.noreply.github.com>
2019-01-23 15:29:16 -0700

    Update states.py

--- states.py
+++ states.py
@@ -19,3 +19,14 @@ class state(metaclass=abc.ABCMeta):
         pass
 
 
+"""
+    There needs to be at least three different main states: Driving, Digging and Dumping
+    that will all be linked into the various motor control and camera operations we will do.
+    Along with those there will need to be states between the main states: Raising and Lowering
+    the arms for the digging wheel etc. Most of these states will also need failsafe controls 
+    that can interupt any state if the actions would be causing harm (eg. the digging wheel 
+    getting lowered into the ground hits something harder than expected, we dont want the actuators
+    burn up or lift the robot off the ground we need to detect if there is a problem and stop the 
+    actuators.) There is a good flow chart on the google drive that has the expected states
+    though they may change over time.
+"""
